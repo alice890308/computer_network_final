03dd90a5f3788588f1fa4d688b65667551f096c3
unknown <alice890308@gmail.com>
2020-01-10 21:48:17 +0800

    add props

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,14 +14,11 @@
       <h2> Temperature value: </h2>
       <p id="temperature">First Temperature value</p>
     </div>
-    <BarChart/>
-	</div>
+      <BarChart :dataset="dataa" :chartoptions="chartoptions"/>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-	#humidity {
-	color:aquamarine;
- }
 </style>
 
 <script>
@@ -29,9 +26,42 @@ import BarChart from "../components/BarChart.vue";
 
 export default {
   name: 'home',
+  AccessToken: "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjlhYjliNzRlM2QxMjM1OGY5MjJmOTZiNDc3ZDIzOWFkMzhkYmJkZDc5OGM5YmIyYmJkYzQ2ZDFmNWUxMTMzMDU3ZTNkZDgyOTA3NjhiMzg5In0.eyJhdWQiOiIyIiwianRpIjoiOWFiOWI3NGUzZDEyMzU4ZjkyMmY5NmI0NzdkMjM5YWQzOGRiYmRkNzk4YzliYjJiYmRjNDZkMWY1ZTExMzMwNTdlM2RkODI5MDc2OGIzODkiLCJpYXQiOjE1NzM0NzE4NjgsIm5iZiI6MTU3MzQ3MTg2OCwiZXhwIjoxNjA1MDk0MjY4LCJzdWIiOiIyNyIsInNjb3BlcyI6W119.ooM1lHo5DBq-F0TbEP4zWQSjcpExXoS_mrG3OeBXajTG-VCWrr7l5fuaTamHF8-FccHY6mE_ckK94mk-eEaevPIdxDqul8kcF9S0fyVhHBYAr17gMkbZIqYDrcW4-G_qTvwnpAslKfqLZ7LRPLstwN8mEhHlnEnM1rDzo64Kj4ZhnvRq6L9ZEtpysNdzTpIsXyNaMcQwNk4Z13VlF-NzcEt7jBbDj8McELrw9KLnldrjZjs0cxv_drKLibIspO89goqARBVGNRR5YD4S4j9jyvup10aa2PPsBjPqb_ojEWkfR7eV5VzfDQLwt-uWQ9EFNj8CWc3PH-UrdLUddsCUFxbBFewHYfiB-xOyU7ek765rWxKpfK8I2DQGcr0uq3U8yobOIX3Uoia9yTJycc48xH0T9Rfc661enTwNQLfMUCQznTAI1HD69-gCl0DCVwB3LbpanGkpJc0aJzWPq-YftqfnVYITH5Jg00Ut1BiDW8gO_mIGH2v7voRPfHVP4oLOTXklJl1_WnqfcI6_gm3l1IpNSPRkP4BwSuSLeyLMkkOscEMmrqODt7iXOBfcNzzpPzoyJRS931sO03jTf_ilM6rahBWrR4oku6QjKVaHXqgpRI6ebeBoNJi2YJrysWyOg0HyxB463HcK4CUiI8KrM8xI-c0XG9e9bX0DAehEdHA",
+  data() {
+    return {
+      dataa: {
+        labels: [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July",
+          "August",
+          "September",
+          "October",
+          "November",
+          "December"
+        ],
+        datasets: [
+          {
+            label: "Data One",
+            backgroundColor: "#fed39f",
+            borderWidth: 4,
+            borderSkipped: false,
+            data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11]
+          }
+        ]
+      },
+      chartoptions: {
+        responsive: true,
+        maintainAspectRatio: false
+      }
+    }
+  },
   components: {
     BarChart
-  },
-  AccessToken: "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjlhYjliNzRlM2QxMjM1OGY5MjJmOTZiNDc3ZDIzOWFkMzhkYmJkZDc5OGM5YmIyYmJkYzQ2ZDFmNWUxMTMzMDU3ZTNkZDgyOTA3NjhiMzg5In0.eyJhdWQiOiIyIiwianRpIjoiOWFiOWI3NGUzZDEyMzU4ZjkyMmY5NmI0NzdkMjM5YWQzOGRiYmRkNzk4YzliYjJiYmRjNDZkMWY1ZTExMzMwNTdlM2RkODI5MDc2OGIzODkiLCJpYXQiOjE1NzM0NzE4NjgsIm5iZiI6MTU3MzQ3MTg2OCwiZXhwIjoxNjA1MDk0MjY4LCJzdWIiOiIyNyIsInNjb3BlcyI6W119.ooM1lHo5DBq-F0TbEP4zWQSjcpExXoS_mrG3OeBXajTG-VCWrr7l5fuaTamHF8-FccHY6mE_ckK94mk-eEaevPIdxDqul8kcF9S0fyVhHBYAr17gMkbZIqYDrcW4-G_qTvwnpAslKfqLZ7LRPLstwN8mEhHlnEnM1rDzo64Kj4ZhnvRq6L9ZEtpysNdzTpIsXyNaMcQwNk4Z13VlF-NzcEt7jBbDj8McELrw9KLnldrjZjs0cxv_drKLibIspO89goqARBVGNRR5YD4S4j9jyvup10aa2PPsBjPqb_ojEWkfR7eV5VzfDQLwt-uWQ9EFNj8CWc3PH-UrdLUddsCUFxbBFewHYfiB-xOyU7ek765rWxKpfK8I2DQGcr0uq3U8yobOIX3Uoia9yTJycc48xH0T9Rfc661enTwNQLfMUCQznTAI1HD69-gCl0DCVwB3LbpanGkpJc0aJzWPq-YftqfnVYITH5Jg00Ut1BiDW8gO_mIGH2v7voRPfHVP4oLOTXklJl1_WnqfcI6_gm3l1IpNSPRkP4BwSuSLeyLMkkOscEMmrqODt7iXOBfcNzzpPzoyJRS931sO03jTf_ilM6rahBWrR4oku6QjKVaHXqgpRI6ebeBoNJi2YJrysWyOg0HyxB463HcK4CUiI8KrM8xI-c0XG9e9bX0DAehEdHA"
+  }
 }
 </script>
