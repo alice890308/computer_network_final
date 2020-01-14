8f17cace920bf3acc9bcaad2bf900f4484d7b141
unknown <alice890308@gmail.com>
2020-01-14 20:54:08 +0800

    add small chart

---
+++ src/components/DayBarChart.vue
@@ -0,0 +1,30 @@
+<script>
+import { Bar } from "vue-chartjs";
+
+export default {
+  extends: Bar,
+  props: {
+    dataset: {
+      type: Object,
+      default: null
+    },
+    chartoptions: {
+      type: Object,
+      default: null
+    },
+    //shouldRender: {
+    //  type: Boolean,
+    //  default: false
+    //}
+  },
+  //watch: {
+  //  shouldRender() {
+  //    this.renderChart(this.dataset, this.chartoptions);
+  //    if (this.shouldRender) this.$emit('completeRender');
+  //  }
+  //},
+  mounted() {
+    this.renderChart(this.dataset, this.chartoptions);
+  }
+};
+</script>
